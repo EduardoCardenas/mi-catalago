ee844b00e611b32e7f7b71e979997c73c936ce1c
EdgarJosh <edgar.ojedaca@uanl.edu.mx>
2020-05-19 18:27:21 -0500

    E3A5

--- README.md
+++ README.md
@@ -3,11 +3,17 @@
 ## Estado actual
 -------------------------------------
 
-![Preview](/preview/E2A4.PNG)
+![Preview](/preview/E3A5.PNG)
 
 ## Versiones anteriores
 
+Etapa 2 Actividad 4
+![Preview](/preview/E2A4.PNG)
+
+-------------------------------------
+Etapa 2 Actividad 3
 ![Preview](/preview/E2A3.PNG)
 
 -------------------------------------
+Etapa 1 Actividad 2
 ![Preview](/preview/E1A2.PNG)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
 import { ViewsComponent } from './views/views.component';
 import { ListComponent } from './list/list.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ViewsComponent,
-    ListComponent
+    ListComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
