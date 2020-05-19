f58192a5ed0ba02e7dbc64be51ca089218e2a419
EdgarJosh <edgar.ojedaca@uanl.edu.mx>
2020-05-19 15:55:01 -0500

    E2 A4

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # Mi catalogo de automoviles
 
 ## Estado actual
----------------------
+-------------------------------------
+
+![Preview](/preview/E2A4.PNG)
+
+## Versiones anteriores
 
 ![Preview](/preview/E2A3.PNG)
+
+-------------------------------------
+![Preview](/preview/E1A2.PNG)
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -15,11 +15,31 @@ export class ListComponent implements OnInit {
   
   closeResult = '';
   constructor(private modalService: NgbModal) { }
+  
+  open(content, auto: Automovil) {
+    this.autoSeleccionado = auto;
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
   ngOnInit(): void {
     this.autos = AUTOMOVILES;
   }
 
   onSelect(auto: Automovil){
-    this.autoSeleccionado = auto;
+    
   }
 }
